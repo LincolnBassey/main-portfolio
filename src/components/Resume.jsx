21eb315e6c132a03a8953ef6a848057fc23013da
LincolnBassey <abbassey706@gmail.com>
2023-12-16 00:50:52 +0100

    resume update

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -26,7 +26,7 @@ export default function Resume() {
         setData(job);
         break;
       case "programming":
-        setData(skill)
+        setData(skill);
         break;
 
       default:
@@ -93,23 +93,64 @@ export default function Resume() {
   ];
 
   const skill = [
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-    { id: "1", skillpercent: "90%", skillname: "HTML njj ", circlePercent:"circle-percent rounded-full  flex items-center justify-center text-center",
-  innercircle:"inner-circle bg-black rounded-full flex items-center justify-center " },
-   
-];
+    {
+      id: "1",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+    {
+      id: "2",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+    {
+      id: "3",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+    {
+      id: "4",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+    {
+      id: "5",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+    {
+      id: "6",
+      skillpercent: "90%",
+      skillname: "HTML njj ",
+      circlePercent:
+        "circle-percent rounded-full  flex items-center justify-center text-center",
+      innercircle:
+        "inner-circle bg-black rounded-full flex items-center justify-center ",
+    },
+  ];
 
   return (
-    <div className="resume flex text-white bg-black items-center justify-center flex-col">
+    <div className="resume flex text-white bg-black items-center md:justify-center md:h-[100vh] flex-col">
       {/*=========== resume header============ */}
       <div
         className="flex pb-10 md:pb-5 gap-2 tracking-widest items-center justify-center flex-col"
@@ -124,7 +165,7 @@ export default function Resume() {
       {/* ============left-right col================ */}
       <div className="left-right bg-red-40 flex md:flex-row flex-col gap-5 items-center justify-center lg:w-[80%] md:w-[90%] w-[95%] md:h-[80vh]  lg:h-[60vh]">
         {/* ===========left col================= */}
-        <div className=" hidden left-col w-1/2  md:w-[250px] items-center justifty-center h-[80%] md:flex">
+        <div className=" hidden left-col w-1/2  md:w-[250px] items-center justifty-center md:h-[50%] lg:h-[70%] md:flex">
           {/* ============left icon========== */}
           <div className="left-icon flex flex-col items-center  justify-center h-full bg-[#0db064b7]  gap-5">
             <div className="">
@@ -154,28 +195,77 @@ export default function Resume() {
           </div>
         </div>
         <div className="w-full md:w-fit">
-          {/* ============Education btn on small screen============== */}
-          <div className="md:hidden w-full bsolute">
-            <button
-              onClick={() => handleclick()}
-              className="border-2 w-full py-2 bg-white text-orange font-bold"
-            >
-              Education
-            </button>
-          </div>
           {/* =======right col============== */}
-          <div
-            className={
-              edu
-                ? "right-col  flex flex-col  gap-3 justify-evenl items-cente text-sm md:w-[350px]  lg:[500px]  h-ful"
-                : " right-col  hidden flex flex-col w-full gap-3 justify-center items-center text-sm"
-            }
-            // ============Education write top============
-          >
+          <div className="right-col  md:flex flex-col hidden gap-3 justify-evenl items-cente text-sm md:w-[350px]  lg:w-[500px]  h-ful">
+            {/* // ============Education write top============ */}
             <div>
+              {data.map((d) => (
+                <div className={" education hidde flex flex-col gap-"}>
+                  <div className="uni flex flex-wrap justify-between">
+                    <h1 className="font-semibold text-orange">{d.school}</h1>
+                    <div className="date px-4 py- rounded-xl bg-orange font-serif">
+                      {d.date}
+                    </div>
+                  </div>
+                  <div className="degree font-semibold">{d.degree}</div>
+                  <div className="degree">
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))}
 
-           
+              {/* ==============work history writetop======== */}
+
+              {data.map((d) => (
+                <div className="work flex flex-col hidde">
+                  <div className="flex flex-wrap justify-between">
+                    <h1 className="font-semibold text-orange">{d.company}</h1>
+                    <div className="date px-4 rounded-xl bg-orange font-serif">
+                      {d.workdate}
+                    </div>
+                  </div>
+                  <div className="position font-semibold">{d.position}</div>
+                  <div>
+                    <p>{d.responsibilty}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* ================programing skill write top============ */}
+
+          <div className=" md:grid grid-cols-3 hidden  gap-">
             {data.map((d) => (
+              <div className="flex flex-col items-center">
+                <div className={d.circlePercent}>
+                  <div className={d.innercircle}>
+                    <span className="percent-num font-bold ">
+                      {d.skillpercent}
+                    </span>
+                  </div>
+                </div>
+                <div className="text-cente font-semibold">
+                  <span>{d.skillname}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+{/* ==============Resume section for small screen============== */}
+
+<div className="md:hidden flex flex-col gap-8">
+  {/* ===================Education============= */}
+        <div className="">
+          <button
+            onClick={() => handleclick()}
+            className="border-2 w-full py-2 bg-white text-orange font-bold"
+          >
+            Education
+          </button>
+  
+          <div className="flex flex-col pt-10 gap-3 text-sm">
+            {education.map((d) => (
               <div className={" education hidde flex flex-col gap-"}>
                 <div className="uni flex flex-wrap justify-between">
                   <h1 className="font-semibold text-orange">{d.school}</h1>
@@ -185,51 +275,97 @@ export default function Resume() {
                 </div>
                 <div className="degree font-semibold">{d.degree}</div>
                 <div className="degree">
-                  <p>{d.text}</p>
+                  <p className="text-[#cecdcd]">{d.text}</p>
                 </div>
               </div>
             ))}
+          </div>
+        </div>
 
-            {/* ==============work history writetop======== */}
 
-            {data.map((d) => (
-              <div className="work flex flex-col hidde">
-                <div className="flex flex-wrap justify-between">
+        {/* =============Work History================ */}
+        <div className="">
+          <button
+            onClick={() => handleclick()}
+            className="border-2 w-full py-2 bg-white text-orange font-bold"
+          >
+            Work History
+          </button>
+  
+          <div className="flex flex-col pt-10 gap-3 text-sm">
+            {job.map((d) => (
+              <div className={" education hidde flex flex-col gap-"}>
+                <div className="uni flex flex-wrap justify-between">
                   <h1 className="font-semibold text-orange">{d.company}</h1>
-                  <div className="date px-4 rounded-xl bg-orange font-serif">
+                  <div className="date px-4 py- rounded-xl bg-orange font-serif">
                     {d.workdate}
                   </div>
                 </div>
-                <div className="position font-semibold">{d.position}</div>
-                <div>
-                  <p>{d.responsibilty}</p>
+                <div className="degree font-semibold">{d.position}</div>
+                <div className="degree">
+                  <p className="text-[#cecdcd]">{d.responsibilty}</p>
                 </div>
               </div>
             ))}
- </div>
-            {/* ================programing skill write top============ */}
+          </div>
+        </div>
+{/* ===============programing skill on small screen============= */}
+        <div className="">
+          <button
+              onClick={() => handleclick()}
+              className="border-2 w-full py-2 bg-white text-orange font-bold"
+            >
+              Programming Skills
+            </button>
 
-            <div className="flex grid grid-cols-3 gap-">
-            {data.map((d) => (
-                <div className="flex flex-col items-center">
-                <div
-                  className={d.circlePercent}
-                >
-                  <div
-                    className={d.innercircle}
-                  >
-                    <span className="percent-num font-bold ">{d.skillpercent}</span>
+            <div className=" grid text-sm grid-cols-3 pt-10 gap-4">
+            {skill.map((d) => (
+              <div className="flex flex-col items-center">
+                <div className={d.circlePercent}>
+                  <div className={d.innercircle}>
+                    <span className="percent-num text-xs ">
+                      {d.skillpercent}
+                    </span>
                   </div>
                 </div>
-                <div className="text-cente font-semibold">
+                <div className="text-cente ">
                   <span>{d.skillname}</span>
                 </div>
               </div>
             ))}
-              </div>
           </div>
+
         </div>
-      </div>
+        <div className="">
+          <button
+              onClick={() => handleclick()}
+              className="border-2 w-full py-2 bg-white text-orange font-bold"
+            >
+              Creative Skills
+            </button>
+
+            <div className=" grid text-sm grid-cols-3 pt-10 gap-4">
+            {skill.map((d) => (
+              <div className="flex flex-col items-center">
+                <div className={d.circlePercent}>
+                  <div className={d.innercircle}>
+                    <span className="percent-num text-xs ">
+                      {d.skillpercent}
+                    </span>
+                  </div>
+                </div>
+                <div className="text-cente ">
+                  <span>{d.skillname}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+        </div>
+
+
+
+</div>
     </div>
   );
 }
